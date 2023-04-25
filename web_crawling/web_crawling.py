d7157255571bb1029a1bf38ad5af1ed2e71e0872
options3224@gmail.com <options3224@gmail.com>
2023-04-25 15:43:00 +0900

    웹 크롤링 네이버 블로그 검색(블로그제목, 글제목 출력)

--- web_crawling/web_crawling.py
+++ web_crawling/web_crawling.py
@@ -5,9 +5,29 @@ base_url = "https://search.naver.com/search.naver?where=view&sm=tab_jum&query="
 keyword = input("검색어를 입력하세요: ")
 search_url = base_url + keyword
 
+# URL 정보 가져오기
 r = requests.get(search_url)
-soup = BeautifulSoup(r.text, "html.parser")
-items = soup.select(".api_txt_lines.total_tit")
 
-for idx, item in enumerate(items):
-    print(f"{idx}: {item.text}")
+# HTML 분석
+soup = BeautifulSoup(r.text, "html.parser")
+
+# 특정 클래스만 선택(맨 앞/띄어쓰기 -> '.' 기호)
+items = soup.select(".total_wrap.api_ani_send")
+
+# 웹 크롤링 결과 출력
+for idx, item in enumerate(items, 1):
+    print("\n<<", idx, ">>")
+
+    # 광고 여부 확인
+    ad = item.select_one(".link_ad")
+    if ad:
+        print("광고입니다.")
+        continue
+
+    # 블로그 제목 출력
+    blog_title = item.select_one(".sub_txt.sub_name").text
+    print(blog_title)
+
+    # 글 제목 출력
+    post_title = item.select_one(".api_txt_lines.total_tit").text
+    print(post_title)
